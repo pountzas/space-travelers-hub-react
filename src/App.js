b0260d668b7b0e56506486a27dced5793ad31010
pountzas <nikos@pountzas.gr>
2021-09-07 01:48:50 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './assets/planet.png';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="80px" />
-        <nav />
-      </header>
+      <Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/missions" />
+          <Route path="/profile" />
+        </Switch>
+      </Router>
     </div>
   );
 }
