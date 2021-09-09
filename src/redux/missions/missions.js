3771bcb5e44ef1bc0b8ef63436c79fdac4aa68d4
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-09-09 10:42:42 -0700

    missions redux setup

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,19 @@
+const UPDATE_MISSIONS = 'space-travelers-hub-react/missions/UPDATE_MISSIONS';
+
+const initialState = [];
+
+export const updateMission = payload => ({
+  type: UPDATE_MISSIONS,
+  payload
+})
+
+const missionReducer =  (state = initialState, action) => {
+  switch (action.type) {
+      case UPDATE_MISSIONS:
+          return action.payload;
+      default:
+          return state;
+      }
+  };
+  
+  export default missionReducer;
