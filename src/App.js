b3fcece54b9755d9f5697a7440271cd76fe68565
pountzas <nikos@pountzas.gr>
2021-09-09 18:53:52 +0300

    add imports for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import logo from './assets/planet.png';
 import Navbar from './components/Navbar';
 import './App.css';
+import Rockets from './components/rockets/rockets';
+import Missions from './components/missions/missions';
+import Profile from './components/profile/profile';
+import getRockets from './redux/slices/rocketsSlice';
 
 function App() {
   return (
