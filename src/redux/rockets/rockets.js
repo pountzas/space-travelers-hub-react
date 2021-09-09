74c5ac38464d2a889c2b1ca71e167a635e78b222
pountzas <nikos@pountzas.gr>
2021-09-09 18:57:24 +0300

    initialize rockets

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,12 @@
+import { GET_ROCKETS, GET_ROCKETS_SUCCESS, GET_ROCKETS_ERR } from '../slices/rocketsSlice';
+
+// Actions
+
+const TOGGLE_RESERVE_ROCKET = 'rockets/rockets/TOGGLE_RESERVE_ROCKET';
+
+// Initial state
+
+const initialState = {
+  rockets: [],
+};
+
