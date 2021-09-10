917ec34ba1a1fbb8494793e2096cb41c57ecac73
pountzas <nikos@pountzas.gr>
2021-09-10 21:08:27 +0300

    Test return initial state of rockets reducer

--- src/tests/rockets.test.js
+++ src/tests/rockets.test.js
@@ -1,1 +1,9 @@
 import rocketsReducer, { toggleReserveRocket } from '../redux/rockets/rockets';
+
+describe('test initial state', () => {
+  test('should return the initial state of rockets reducer', () => {
+    expect(rocketsReducer(undefined, {})).toEqual({
+      rockets: [],
+    });
+  });
+});
