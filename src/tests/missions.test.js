103ceac9aa604619df003ca40d0fe3e1ed792ec2
pountzas <nikos@pountzas.gr>
2021-09-10 21:33:52 +0300

    Test return initial state of missions reducer

--- src/tests/missions.test.js
+++ src/tests/missions.test.js
@@ -1,1 +1,9 @@
 import missionsReducer, { toggleMission } from '../redux/missions/missions';
+
+describe('test initial state', () => {
+  test('should return the initial state of missions reducer', () => {
+    expect(missionsReducer(undefined, {})).toEqual({
+      missions: [],
+    });
+  });
+});
